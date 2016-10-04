587117a57f4b318b4baed983644695c29a51c1c2
sgordon007 <sgordon@lbl.gov>
2016-10-03 20:49:11 -0700

    modifying gff2sort.py for hybridum analysis

--- OtherFiles/AdditionalAnalysisScripts/gff2sort.py
+++ OtherFiles/AdditionalAnalysisScripts/gff2sort.py
@@ -2,9 +2,12 @@
 #Chr1	phytozomev11	mRNA	2903	10817	.	+	.	ID=LOC_Os01g01010.1.MSUv7.0;Name=LOC_Os01g01010.1;pacid=33123311;longest=1;Parent=LOC_Os01g01010.MSUv7.0
 #Pahal.C01314.1	Chr03	7436628	7436867	80	1	U	1007309
 
-gffFiles= ['q.PAC4GC.465.gff3']#,'t.PAC2_0.167.gff3','t.PAC2_0.275.gff3','t.PAC2_0.285.gff3']
+#gffFiles= ['q.PAC4GC.465.gff3']#,'t.PAC2_0.167.gff3','t.PAC2_0.275.gff3','t.PAC2_0.285.gff3']
 
 #gffFiles = ['q.PAC4GC.524.gff3','q.PAC4GC.523.gff3'] #189 311 296
+
+gffFiles= ['t.PAC2_0.313.gff3', 't.PAC2_0.323.gff3', 't.PAC2_0.308.gff3', 't.PAC2_0.314.gff3', 't.PAC2_0.312.gff3', 't.PAC2_0.316.gff3', 't.PAC2_0.383.gff3']  
+
 # Rename files like Phallii_308_v2.0.gene.gff3 to 't.PAC2_0.308.gff3' <---Target species
 # if original gff file is query species, rename to q.PAC4GC.xxx.gff or ...xxx.gff3 instead of t.PAC2_0.308...etc
 def gff2sort2(gffFiles):
@@ -29,6 +32,7 @@ gff2sort2(gffFiles)
 
     # code used specifically for polyploid sequencing
     # please email Joshua Levy for more info
+    # for final hybridum this will be: q.PAC4GC.001.gff3	q.PAC4GC.002.gff3
 """
 
 outFileNameN='q.PAC4GC.523.sort'
